55213ea537f24509742e03d1f256063c6fe3efc3
Tobi <lightszentip@gmail.com>
2022-12-22 15:49:26 +0100

    test: optimize test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,36 +10,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-
-
-        echo "dasdsads";
-        echo file_exists(resource_path('.changes'));
-        //changelog.json
-        if (file_exists(resource_path('.changes'))) {
-            array_map('unlink', glob(resource_path('.changes').DIRECTORY_SEPARATOR.'*.*'));
-            rmdir(resource_path('.changes'));
-        }
-        mkdir(resource_path('.changes'));
-
-        copy(
-            __DIR__.''.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'.changes'.DIRECTORY_SEPARATOR.'changelog.json',
-            resource_path('.changes'.DIRECTORY_SEPARATOR.'changelog.json')
-        );
-
-
-        //version.yml
-        if (file_exists(resource_path('.version'))) {
-            array_map('unlink', glob(resource_path('.version').DIRECTORY_SEPARATOR.'*.*'));
-            rmdir(resource_path('.version'));
-        }
-        mkdir(resource_path('.version'));
-
-        copy(
-            __DIR__.''.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'.version'.DIRECTORY_SEPARATOR.'version.yml',
-            resource_path('.version'.DIRECTORY_SEPARATOR.'version.yml')
-        );
-
     }
 
     protected function getPackageProviders($app): array
