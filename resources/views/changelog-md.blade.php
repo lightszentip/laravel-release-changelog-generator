0c0a39a70d77884aca478793d8fc7ee71f085df8
Robert <robert@codepeak.se>
2023-03-06 17:20:41 +0100

    Work with array data instead in template

--- resources/views/changelog-md.blade.php
+++ resources/views/changelog-md.blade.php
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 @foreach ($changelog as $release => $changelogItems)
-## [{{ ucfirst($release) }}]@if ($changelogItems->date) - {{ \Carbon\Carbon::parse($changelogItems->date)->format('Y-m-d') }} @endif
+## [{{ ucfirst($release) }}]@if ($changelogItems['date']) - {{ \Carbon\Carbon::parse($changelogItems['date'])->format('Y-m-d') }} @endif
 
 @foreach ($changelogItems as $changeType => $items)
 @if (is_array($items))
@@ -13,7 +13,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 ### {{ ucfirst($changeType) }}
 
 @foreach ($items as $item)
-- {{ $item->message }}
+- {{ $item['message'] }}
 @endforeach
 @endif
 @endforeach
